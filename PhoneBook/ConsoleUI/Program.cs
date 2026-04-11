ec3ed97cda2b298c7343225b7b5357528a85f090
jzhartman <jzhartman@gmail.com>
2026-04-10 21:19:49 -0400

    Add project files.

---
+++ PhoneBook/ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Presentation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ PhoneBook/Domain/Entities/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneBook.Domain.Entities;
+
+internal class Contact
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
