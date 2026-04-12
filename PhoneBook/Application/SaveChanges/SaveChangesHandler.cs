f874021847c8d68d2d74f8b0c75919d196f6bbfc
jzhartman <jzhartman@gmail.com>
2026-04-11 23:19:04 -0400

    Created use ase for save changes

---
+++ PhoneBook/Application/SaveChanges/SaveChangesHandler.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBook.Application.Interfaces;
+
+namespace PhoneBook.Application.SaveChanges;
+
+internal class SaveChangesHandler
+{
+    private readonly IContactRepository _repo;
+
+    public SaveChangesHandler(IContactRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task HandleAsync()
+    {
+        await _repo.SaveChangesAsync();
+    }
+}
