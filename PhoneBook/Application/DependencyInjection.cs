b0ba040bd37e41d1a946909d2523a8a898892a96
jzhartman <jzhartman@gmail.com>
2026-04-11 23:19:16 -0400

    DI wire up of use cases

---
+++ PhoneBook/Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PhoneBook.Application.AddContact;
+using PhoneBook.Application.SaveChanges;
+
+namespace PhoneBook.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient<AddContactHandler>();
+        services.AddTransient<SaveChangesHandler>();
+
+        return services;
+    }
+}
