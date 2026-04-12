714ab7e35205339553abe797dc2c97a0281b5282
jzhartman <jzhartman@gmail.com>
2026-04-11 23:18:52 -0400

    Creates use case for Add Contact

---
+++ PhoneBook/Application/AddContact/AddContactHandler.cs
@@ -0,0 +1,26 @@
+﻿using PhoneBook.Application.Interfaces;
+using PhoneBook.Domain.Entities;
+
+namespace PhoneBook.Application.AddContact;
+
+internal class AddContactHandler
+{
+    private readonly IContactRepository _repo;
+
+    public AddContactHandler(IContactRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task HandleAsync(AddContactRequest contact)
+    {
+        await _repo.AddAsync(
+            new Contact
+            {
+                FirstName = contact.FirstName,
+                LastName = contact.LastName,
+                Email = contact.Email,
+                PhoneNumber = contact.PhoneNumber
+            });
+    }
+}
---
+++ PhoneBook/Application/AddContact/AddContactRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.Application.AddContact;
+
+public record AddContactRequest
+(
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    string Email
+);
