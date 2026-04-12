41d7df3406dec1a801875d23f6e438f28858aefa
jzhartman <jzhartman@gmail.com>
2026-04-11 23:18:07 -0400

    Added seed data to context

--- PhoneBook/Infrastructure/PhoneBookDbContext.cs
+++ PhoneBook/Infrastructure/PhoneBookDbContext.cs
@@ -15,5 +15,42 @@ public class PhoneBookDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Contact>().ToTable("Contacts");
+
+        modelBuilder.Entity<Contact>()
+            .HasData(new List<Contact>
+            {
+                new Contact
+                {
+                    ContactId = 1,
+                    FirstName = "Malcolm",
+                    LastName = "Reynolds",
+                    Email = "mal@serenity.com",
+                    PhoneNumber = "1111111111"
+                },
+                new Contact
+                {
+                    ContactId = 2,
+                    FirstName = "Inara",
+                    LastName = "Serra",
+                    Email = "inara@serenity.com",
+                    PhoneNumber = "2222222222"
+                },
+                new Contact
+                {
+                    ContactId = 3,
+                    FirstName = "Shepherd",
+                    LastName = "Book",
+                    Email = "book@serenity.com",
+                    PhoneNumber = "3333333333"
+                },
+                new Contact
+                {
+                    ContactId = 4,
+                    FirstName = "Jayne",
+                    LastName = "Cobb",
+                    Email = "vera@serenity.com",
+                    PhoneNumber = "4444444444"
+                }
+            });
     }
 }
