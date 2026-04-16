a332aa6ae70b34ea7ce5f1d9b75a82319014b360
jzhartman <jzhartman@gmail.com>
2026-04-15 23:26:37 -0400

    Created EditContact use case

---
+++ PhoneBook/Application/EditContact/EditContactHandler.cs
@@ -0,0 +1,27 @@
+﻿using PhoneBook.Application.DTOs;
+using PhoneBook.Application.Interfaces;
+using PhoneBook.Domain.Entities;
+
+namespace PhoneBook.Application.EditContact;
+
+public class EditContactHandler
+{
+    private readonly IContactRepository _repo;
+
+    public EditContactHandler(IContactRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public async Task HandleAsync(ContactResponse contact)
+    {
+        await _repo.UpdateAsync(new Contact
+        {
+            ContactId = contact.ContactId,
+            FirstName = contact.FirstName,
+            LastName = contact.LastName,
+            PhoneNumber = contact.PhoneNumber,
+            Email = contact.Email
+        });
+    }
+}
