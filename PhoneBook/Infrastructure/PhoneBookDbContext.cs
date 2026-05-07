af0bb5a509347f0a25756bb06bfbd81daa583dd7
jzhartman <jzhartman@gmail.com>
2026-05-06 21:36:53 -0400

    Rebuilt original database before generation of  migration

--- PhoneBook/Infrastructure/PhoneBookDbContext.cs
+++ PhoneBook/Infrastructure/PhoneBookDbContext.cs
@@ -6,7 +6,7 @@ namespace PhoneBook.Infrastructure;
 public class PhoneBookDbContext : DbContext
 {
     public DbSet<Contact> Contacts { get; set; }
-    public DbSet<ContactCategory> ContactCategories { get; set; }
+    //public DbSet<ContactCategory> ContactCategories { get; set; }
 
     public PhoneBookDbContext(DbContextOptions<PhoneBookDbContext> options) : base(options)
     {
@@ -15,48 +15,75 @@ public class PhoneBookDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<ContactCategory>().ToTable("ContactCategories");
+        //modelBuilder.Entity<ContactCategory>().ToTable("ContactCategories");
         modelBuilder.Entity<Contact>().ToTable("Contacts");
 
-        //modelBuilder.Entity<Contact>()
-        //    .HasData(new List<Contact>
-        //    {
-        //        new Contact
-        //        {
-        //            ContactId = 1,
-        //            FirstName = "Malcolm",
-        //            LastName = "Reynolds",
-        //            PhoneNumber = "1111111111",
-        //            Email = "mal@serenity.com",
-        //            CategoryId = 1
-        //        },
-        //        new Contact
-        //        {
-        //            ContactId = 2,
-        //            FirstName = "Inara",
-        //            LastName = "Serra",
-        //            PhoneNumber = "2222222222",
-        //            Email = "inara@serenity.com",
-        //            CategoryId = 1
-        //        },
-        //        new Contact
-        //        {
-        //            ContactId = 3,
-        //            FirstName = "Shepherd",
-        //            LastName = "Book",
-        //            PhoneNumber = "3333333333",
-        //            Email = "book@serenity.com",
-        //            CategoryId = 1
-        //        },
-        //        new Contact
-        //        {
-        //            ContactId = 4,
-        //            FirstName = "Jayne",
-        //            LastName = "Cobb",
-        //            PhoneNumber = "4444444444",
-        //            Email = "vera@serenity.com",
-        //            CategoryId = 1
-        //        }
-        //    });
+        modelBuilder.Entity<Contact>()
+            .HasData(new List<Contact>
+            {
+                new Contact
+                {
+                    Id = 1,
+                    FirstName = "Malcolm",
+                    LastName = "Reynolds",
+                    PhoneNumber = "1111111111",
+                    Email = "browncoat@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 2,
+                    FirstName = "Inara",
+                    LastName = "Serra",
+                    PhoneNumber = "2222222222",
+                    Email = "companion@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 3,
+                    FirstName = "Shepherd",
+                    LastName = "Book",
+                    PhoneNumber = "3333333333",
+                    Email = "book@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 4,
+                    FirstName = "Jayne",
+                    LastName = "Cobb",
+                    PhoneNumber = "4444444444",
+                    Email = "vera@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 5,
+                    FirstName = "Kaylee",
+                    LastName = "Frye",
+                    PhoneNumber = "5555555555",
+                    Email = "mechanic@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 6,
+                    FirstName = "Simon",
+                    LastName = "Tam",
+                    PhoneNumber = "6666666666",
+                    Email = "awesomeDoctor@serenity.com",
+                    //CategoryId = 1
+                },
+                new Contact
+                {
+                    Id = 7,
+                    FirstName = "River",
+                    LastName = "Tam",
+                    PhoneNumber = "7777777777",
+                    Email = "miranda@serenity.com",
+                    //CategoryId = 1
+                }
+            });
     }
 }
