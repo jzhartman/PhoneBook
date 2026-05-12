07eee8263982d932f0d58788634d6f3841d82084
jzhartman <jzhartman@gmail.com>
2026-05-11 23:05:24 -0400

    Added subfolders for ConsoleUI services

---
+++ PhoneBook/ConsoleUI/Services/Categories/LookupCategoryMenuService.cs
@@ -0,0 +1,107 @@
+﻿using PhoneBook.Application.Categories.DTOs;
+using PhoneBook.Application.Categories.GetCategoryById;
+using PhoneBook.ConsoleUI.Enums;
+using PhoneBook.ConsoleUI.Input;
+using PhoneBook.ConsoleUI.Output;
+using PhoneBook.ConsoleUI.Services.Categories;
+using PhoneBook.Domain.Validation;
+using PhoneBook.Domain.Validation.Errors;
+using Spectre.Console;
+
+namespace PhoneBook.ConsoleUI.Services;
+
+internal class LookupCategoryMenuService
+{
+    private readonly CategorySelectionService _categorySelectionService;
+    private readonly GetCategoryByIdHandler _getCategoryByIdHandler;
+    private readonly Messages _messages;
+    private readonly UserInput _userInput;
+
+    public LookupCategoryMenuService(CategorySelectionService categorySelectionService, GetCategoryByIdHandler getCategoryByIdHandler,
+                                    Messages messages, UserInput userInput)
+    {
+        _categorySelectionService = categorySelectionService;
+        _getCategoryByIdHandler = getCategoryByIdHandler;
+
+        _messages = messages;
+        _userInput = userInput;
+    }
+
+    internal async Task RunAsync()
+    {
+        bool returnToMainMenu = false;
+        LookupMenuOptions[] menuOptions = Enum.GetValues<LookupMenuOptions>();
+
+        Console.Clear();
+        var categegoryResult = await _categorySelectionService.RunAsync();
+
+        if (categegoryResult.IsFailure || categegoryResult.Value is null)
+        {
+            _messages.ErrorMessage(categegoryResult.Errors);
+            _userInput.PressAnyKeyToContinue();
+            return;
+        }
+
+        while (returnToMainMenu == false)
+        {
+            Console.Clear();
+
+            AnsiConsole.WriteLine($"Viewing contacts in category {categegoryResult.Value.Name}:");
+            AnsiConsole.WriteLine();
+            //_contactDetailsView.Render(contactResult.Value);
+            AnsiConsole.WriteLine();
+
+            RenderContactDetailKeyOptions();
+
+            var keyInfo = Console.ReadKey(true);
+            var operation = await ManageKeyPressMenu(keyInfo, categegoryResult.Value);
+
+            if (operation == LookupMenuOptions.Exit || operation == LookupMenuOptions.Delete) returnToMainMenu = true;
+            if (operation == LookupMenuOptions.Update) categegoryResult = await _getCategoryByIdHandler.HandleAsync(categegoryResult.Value.Id);
+
+            if (categegoryResult.IsFailure)
+            {
+                _messages.ErrorMessage(new[] { Errors.LoadEditDataFailed });
+                _userInput.PressAnyKeyToContinue();
+            }
+        }
+    }
+
+
+    private void RenderContactDetailKeyOptions()
+    {
+        var table = new Table()
+                        .RoundedBorder()
+                        .BorderColor(Spectre.Console.Color.Blue)
+                        .ShowRowSeparators();
+
+        table.AddColumn("Key");
+        table.AddColumn("Operation");
+
+        table.AddRow("D", "Delete Contact");
+        table.AddRow("E", "Edit Contact");
+        table.AddRow("M", "Return to Main Menu");
+
+        AnsiConsole.Write(table);
+    }
+
+    private async Task<LookupMenuOptions> ManageKeyPressMenu(ConsoleKeyInfo keyInfo, CategoryResponse contact)
+    {
+        switch (keyInfo.Key)
+        {
+            case ConsoleKey.D:
+                //await _deleteContactService.RunAsync(contact);
+                return LookupMenuOptions.Delete;
+            case ConsoleKey.E:
+                //await _editContactService.RunAsync(contact);
+                return LookupMenuOptions.Update;
+            case ConsoleKey.M:
+                return LookupMenuOptions.Exit;
+            default:
+                _messages.ErrorMessage(new[] { new Error("Input", "Invalid key press") });
+                Console.WriteLine("ERROR! Invalid key press");
+                _userInput.PressAnyKeyToContinue();
+                return LookupMenuOptions.Unknown;
+        }
+    }
+}
---
+++ PhoneBook/ConsoleUI/Services/Contacts/AddContactService.cs
@@ -0,0 +1,73 @@
+﻿using PhoneBook.Application.Contacts.AddContact;
+using PhoneBook.Application.Contacts.SaveChanges;
+using PhoneBook.ConsoleUI.Input;
+using PhoneBook.ConsoleUI.Output;
+using PhoneBook.ConsoleUI.Services.Categories;
+using PhoneBook.Domain.Validation;
+using PhoneBook.Domain.Validation.Errors;
+
+namespace PhoneBook.ConsoleUI.Services.Contacts;
+
+internal class AddContactService
+{
+    private readonly CategorySelectionService _categorySelectionService;
+    private readonly AddContactHandler _addContactHandler;
+    private readonly SaveChangesHandler _saveChangesHandler;
+    private readonly UserInput _userInput;
+    private readonly Messages _messages;
+
+    public AddContactService(AddContactHandler addContactHandler, SaveChangesHandler saveChangesHandler,
+                                CategorySelectionService categorySelectionService, UserInput userInput, Messages messages)
+    {
+        _categorySelectionService = categorySelectionService;
+        _addContactHandler = addContactHandler;
+        _saveChangesHandler = saveChangesHandler;
+        _userInput = userInput;
+        _messages = messages;
+    }
+
+    internal async Task RunAsync()
+    {
+        var firstName = _userInput.GetNameFromUser($"Enter your [green]FIRST NAME[/]:");
+        var lastName = _userInput.GetNameFromUser($"Enter your [green]LAST NAME[/]:");
+        var email = _userInput.GetEmailAddressFromUser();
+        var phoneNumber = _userInput.GetPhoneNumberFromUser();
+        var categoryId = await GetCategoryIdFromUser();
+
+        var addResult = await _addContactHandler.HandleAsync(new(firstName, lastName, phoneNumber, email, categoryId));
+        var errors = new List<Error>();
+
+        if (addResult.IsSuccess)
+        {
+            var saveResult = await _saveChangesHandler.HandleAsync();
+
+            if (saveResult is null)
+                errors.Add(Errors.SaveResponseNull);
+
+            else if (saveResult.IsFailure)
+                errors.AddRange(saveResult.Errors);
+
+            else if (saveResult.IsSuccess)
+                return;
+        }
+
+        if (addResult.IsFailure)
+            errors.AddRange(addResult.Errors);
+
+        if (addResult is null)
+            errors.Add(Errors.AddResponseNull);
+
+        _messages.ErrorMessage(errors);
+        _userInput.PressAnyKeyToContinue();
+    }
+
+    private async Task<int> GetCategoryIdFromUser()
+    {
+        var categoryResponse = await _categorySelectionService.RunAsync();
+
+        if (categoryResponse is null || categoryResponse.Value is null || categoryResponse.IsFailure)
+            return 0;
+
+        return categoryResponse.Value.Id;
+    }
+}
