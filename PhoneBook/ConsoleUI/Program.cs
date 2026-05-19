c761351112b82d7812bcb9aa6d8b45174eadfbb9
jzhartman <jzhartman@gmail.com>
2026-05-18 21:18:03 -0400

    Removed logging output from console window

--- PhoneBook/ConsoleUI/Program.cs
+++ PhoneBook/ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using PhoneBook.Application;
 using PhoneBook.ConsoleUI;
 using PhoneBook.ConsoleUI.Services;
@@ -9,9 +10,14 @@ namespace PhoneBook.Presentation;
 
 internal class Program
 {
-    static async Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
         var host = Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddDebug();
+            })
             .ConfigureServices((context, services) =>
             {
                 services.AddPresentation();
