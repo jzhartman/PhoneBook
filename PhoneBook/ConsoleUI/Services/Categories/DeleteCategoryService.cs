f7d4579331ef47e2e1534ac37f2a5b3eeaacd07b
jzhartman <jzhartman@gmail.com>
2026-05-25 23:51:46 -0400

    Prep for next step: DeleteCategoryService

---
+++ PhoneBook/ConsoleUI/Services/Categories/DeleteCategoryService.cs
@@ -0,0 +1,32 @@
+﻿using PhoneBook.Application.Categories.DeleteCategory;
+
+namespace PhoneBook.ConsoleUI.Services.Categories;
+
+internal class DeleteCategoryService
+{
+    private readonly DeleteCategoryByIdHandler _deleteCategoryByIdHandler;
+
+    public DeleteCategoryService(DeleteCategoryByIdHandler deleteCategoryByIdHandler)
+    {
+        _deleteCategoryByIdHandler = deleteCategoryByIdHandler;
+    }
+
+    internal async Task RunAsync()
+    {
+        // Print category list and let user select one
+        // Pass category to method below
+        var result = await _deleteCategoryByIdHandler.HandleAsync();
+
+        // Handle errors
+        // Confirmation??
+
+        Console.WriteLine("Running...");
+        Console.Write("Press any key");
+        Console.ReadLine();
+    }
+
+    // ToDo: Left off here.
+    /*  Need to flesh this out better
+     *  Review error handling and validation in use case
+     */
+}
